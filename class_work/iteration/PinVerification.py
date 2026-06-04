d0a32fe8c8b47202fc15c3bc9cec9d77f711d80f
Lucky Rawat <official1850lucky.com>
2026-06-04 12:07:44 +0530

    verify the pin

--- class_work/iteration/PinVerification.py
+++ class_work/iteration/PinVerification.py
@@ -1,4 +1,6 @@
-pin = int(input("Enter the PIN:")) 
+pin = int(input("Enter the PIN:"))
+if(pin.len()!=4):
+    exit("PIN must be 4 digits long.")
 while pin != 1234:
     print("Incorrect PIN. Tryagain.")
     pin = int(input())  
