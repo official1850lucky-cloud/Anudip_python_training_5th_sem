fcdd06a33145b2ccdf35c566ac75878a95965d26
Lucky Rawat <official1850lucky.com>
2026-06-05 12:02:07 +0530

    Clean up and sync deleted files

--- class_work/iteration/PinVerification.py
+++
@@ -1,7 +0,0 @@
-pin = int(input("Enter the PIN:"))
-if(pin.len()!=4):
-    exit("PIN must be 4 digits long.")
-while pin != 1234:
-    print("Incorrect PIN. Tryagain.")
-    pin = int(input())  
-print("Access Granted")
--- lab_work/iteration_4thJune_lab/NumberPyramid.py
+++
@@ -1,7 +0,0 @@
-# Accept number of rows
-rows = int(input("Enter number of rows: "))
-# Print pattern
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print(j, end="")
-    print()
